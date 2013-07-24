689fee77b68122771d7c426c195c11b5d0345ab9
Roy van de Water <dev+roy@integrumtech.com>
2013-07-24 11:32:49 -0700

    Add grails files

---
+++ grails-app/domain/met/someone/Meeting.groovy
@@ -0,0 +1,7 @@
+package met.someone
+
+class Meeting {
+
+    static constraints = {
+    }
+}
---
+++ test/unit/met/someone/MeetingTests.groovy
@@ -0,0 +1,17 @@
+package met.someone
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Meeting)
+class MeetingTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
