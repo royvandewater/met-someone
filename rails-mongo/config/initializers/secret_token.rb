a9d4ee7f3e7b6b6040fadb75b8880f7225e963f5
Roy van de Water <dev+roy@integrumtech.com>
2013-08-01 15:13:07 -0700

    Add base rails-mongo files

---
+++ rails-mongo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MetSomeone::Application.config.secret_key_base = 'f8cefaac8a826b672ca2ee2b497f5be99136522ece69c139c845d3a78505bb26157057b3640bae104dc98560cb78306a9996f64d10ddade104248f7f34080536'
---
+++ rails-mongo/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MetSomeone::Application.config.session_store :cookie_store, key: '_met-someone_session'
