5e599db6c5eed88752ea1410ab3995d3aac2a085
Roy van de Water <dev+roy@integrumtech.com>
2013-08-01 13:10:17 -0700

    Add a new Noir base project

---
+++ noir/src/met_someone/views/common.clj
@@ -0,0 +1,12 @@
+(ns met-someone.views.common
+  (:use [noir.core :only [defpartial]]
+        [hiccup.page :only [include-css html5]]))
+
+(defpartial layout [& content]
+            (html5
+              [:head
+               [:title "met-someone"]
+               (include-css "/css/reset.css")]
+              [:body
+               [:div#wrapper
+                content]]))
